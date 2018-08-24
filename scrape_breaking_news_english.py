1c566207eef9d906e7492b6e6dcf9d52335ec55c
dzvinka <yarish@ucu.edu.ua>
2018-08-24 09:50:32 +0200

    fixed output data format

--- scrape_breaking_news_english.py
+++ scrape_breaking_news_english.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 URL = 'https://breakingnewsenglish.com/'
 MAIN = 'graded-news-stories.html'
 
-data = []
+data = {'level1': [], 'level2': [], 'level3': []}
 
 try:
     page = requests.get(URL + MAIN)
@@ -14,30 +14,28 @@ try:
     # print(soup.prettify())
     art_links = [a.get('href').split('.')[0] for a in soup.find('div', id='primary').find_all('a')]
     print(len(art_links))
-    for a_l in art_links[3:6]:
-        d = {}
+    for a_l in art_links[3:]:
+        d = []
         for i in range(3):
             page = requests.get(URL + a_l + '-' + str(i) + '.html')
             if page.status_code != 200:
                 print('ble')
-                d = {}
+                d = []
                 break
 
             art_soup = BeautifulSoup(page.content, 'html.parser')
 
             article = art_soup.findAll('article')[0].getText().split('\n')
             # print(article)
-            d['level %s' % i] = [p for p in article if p and not p.startswith('(')]
+            d.append(''.join([p for p in article if p and not p.startswith('(')]))
 
-        print(d)
         if d:
-            data.append(d)
-
-
-
+            for k in range(len(d)):
+                data['level%d' % (k + 1)].append(d[k])
+        print(len(data['level1']))
 
 finally:
     with open('data/data_breaking_news_english.json', 'w') as f:
-        f.write(json.dumps({'breaking_news_english': data}))
+        f.write(json.dumps(data))
 
 
--- scrape_news_in_levels.py
+++ scrape_news_in_levels.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 URL = 'https://www.newsinlevels.com/page/'
 PAGES = 203
 
-data = []
+data = {'level1': [], 'level2': [], 'level3': []}
 used_links = []
 try:
     for j in range(1, PAGES):
@@ -18,24 +18,26 @@ try:
 
         for l in links:
 
-            d = {}
+            d = []
             for i in range(3):
                 if l[i] in used_links:
                     break
                 page = requests.get(l[i])
                 art_soup = BeautifulSoup(page.content, 'html.parser')
                 try:
-                    d['level %s' % i] = [p.getText() for p in art_soup.find_all('div', id='nContent')[0].find_all('p')[1:-2]]
+                    d.append(''.join([p.getText() for p in art_soup.find_all('div', id='nContent')[0].find_all('p')[1:-2]]))
 
                 except IndexError:
+                    d = []
                     print('skipped 1 article')
                     break
             if d:
-                data.append(d)
+                for k in range(len(d)):
+                    data['level%d' % (k + 1)].append(d[k])
             used_links.extend(l)
-        print(data)
+            print(len(data['level1']))
 finally:
     with open('data/data_news_in_levels.json', 'w') as f:
-        f.write(json.dumps({'news_in_levels': data}))
+        f.write(json.dumps(data))
 
 
